6852560a74580c71602ceb2c9836b5d412372132
Reagan <rokhanga@gmail.com>
2023-08-22 19:50:02 +0300

    modify article component

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Header name={blogData.name} />
       <About image={blogData.image} about={blogData.about} />
-      <ArticleList post={blogData.posts} />
+      <ArticleList posts={blogData.posts} />
      
       {/* You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck! */}
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,21 +4,25 @@ const ArticleList = (props) => {
     // const article = props.post.map(() => { return <li key={article.id}> {article.title}</li>})
   return (
     <main>
-        <Article title={props.post[0].title} date={props.post[0].date} preview={props.post[0].preview} />
+        {/* <Article title={props.post[0].title} date={props.post[0].date} preview={props.post[0].preview} />
         <Article title={props.post[1].title} date={props.post[1].date} preview={props.post[1].preview} />
-        <Article title={props.post[2].title} date={props.post[2].date} preview={props.post[2].preview} />
+        <Article title={props.post[2].title} date={props.post[2].date} preview={props.post[2].preview} /> */}
+        <Article posts = {props.posts} />
     </main>
   )
 }
 
 
-const Article = ({title, date="January 1, 1970", preview}) => {
+const Article = (props) => {
     return (
         <article>
-            <h3>{title}</h3>
-            <small>{date}</small>
-            <p>{preview}</p>
-        </article>
+            {props.posts.map((post) =>(
+                <div key={post.id}>
+               <h3>{post.title}</h3> 
+               <small>{post.date}</small>
+               <p>{post.preview}</p>
+            </div> ))
+ } </article>
     )
 }
 
